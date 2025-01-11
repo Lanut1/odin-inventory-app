98dc0265deceef6524469a8db7a0cfcea3302875
Lanut1 <anna.tabakova27@gmail.com>
2025-01-11 16:59:15 +0100

    Implement user registration

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,45 @@
 const db = require("../db/queries");
 require('dotenv').config();
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 function registerPageGet(req, res) {
-  res.render("registrationPage");
+  res.render("registrationPage", {
+    username: null,
+    email: null,
+    errors: null
+  });
 }
 
-async function registerPagePost(req, res) {
+async function registerPagePost(req, res, next) {
+  try {
+    const { username, email, password } = req.body;
 
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).render("registrationPage", {
+        username,
+        email,
+        errors: errors.array()
+      })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = {
+      username,
+      email,
+      password_hash: hashedPassword,
+      status: "user"
+    }
+
+    await db.registerUser(newUser);
+
+    res.redirect("/auth/login");
+  } catch (error) {
+    next(error);
+  }
 }
 
 function loginPageGet(req, res) {
