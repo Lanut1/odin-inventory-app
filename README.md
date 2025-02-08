861a09ce461e8ccb8b17a0d5014e8634393dd7cf
Lanut1 <anna.tabakova27@gmail.com>
2025-02-08 22:04:22 +0100

    Update README and populatedb script

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
 # Odin Inventory PROCosmetics App
 
 ### Description
-The Odin Inventory PROCosmetics App is a web application built to manage cosmetic product inventories. It allows users to view, add, and manage products using a simple and intuitive interface. This project was created as part of [The Odin Project](https://www.theodinproject.com/) curriculum to learn the fundamentals of building full-stack web applications.
+PROCosmetics App is a web application built to manage cosmetic product inventories and allow users to browse products, write, and view comments. It provides a simple and intuitive interface for managing cosmetic products while offering the functionality for users to engage with the products through reviews. This project was created as part of [The Odin Project](https://www.theodinproject.com/) curriculum to learn the fundamentals of building full-stack web applications.
 
 ### Features
-- View a list of all products.
-- Add new products with relevant details.
-- Update and delete cosmetic products.
+- Users can register, log in, and authenticate with Passport.js. Passwords are stored securely using bcrypt.
+- Logged-in users can write and view comments about products.
+- Admin users can add new products and update them, delete products and comments.
 - Basic validation for product inputs.
 - MVC architecture for clean and maintainable code.
 - Integration with PostgreSQL for data storage.
 - Deployment on Railway for easy access.
+- The design is inspired by Figma templates from [Nickel Fox](https://www.figma.com/@nickelfox) and [UIUX Expert(Aashifa)](https://www.figma.com/@uiux_expert)
+- Images are sourced from [Freepik](https://www.freepik.com/serie/13863102), [Way Home Studio](https://www.freepik.com/author/wayhomestudio) and the official websites of [Avene](https://www.eau-thermale-avene.com/) and [Bioderma](https://www.bioderma.com/). Products images have been uploaded to Cloudinary for efficient management and display.
+- The app includes a responsive mobile version, optimized for smaller screens, ensuring smooth navigation and user experience on mobile devices.
 
 ### Technologies Used
 - **Backend**: Express.js
 - **Database**: PostgreSQL
+- **Authentication**: Passport.js 
 - **Architecture**: MVC Pattern
 - **Validation**: Express Validator for input validation
 - **Templating Engine**: EJS for rendering views
@@ -64,10 +68,10 @@ The application is deployed on [Railway](https://railway.app/). You can access t
 - [Live Application](https://odin-inventory-app-production-6ef5.up.railway.app/)
 
 ### Future Enhancements
-- Add user authentication for better access control.
 - Implement pagination for the products list.
 - Add image optimization for faster loading.
-- Improve UI design for a better user experience.
+- Create user profiles where users can manage their information and preferences.
+- Allow users to add their own photos and upload pictures to their comments.
 
 ### License
 This project is licensed under the MIT License.
--- db/populatedb.js
+++ db/populatedb.js
@@ -30,6 +30,24 @@ CREATE TABLE IF NOT EXISTS products (
   FOREIGN KEY (skintype_id) REFERENCES skintype(id)
 );
 
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  username VARCHAR(255) NOT NULL UNIQUE,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password_hash TEXT NOT NULL,
+  status VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS reviews (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  message TEXT NOT NULL,
+  user_id INTEGER,
+  product_id INTEGER NOT NULL,
+  time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL,
+  FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
+);
+
 INSERT INTO brand (name) VALUES
   ('BIODERMA');
 
@@ -47,17 +65,17 @@ INSERT INTO skintype (name) VALUES
   ('Combination to oily');
 
 INSERT INTO products (name, description, photo_url, brand_id, category_id, skintype_id) VALUES
-  ('Sensibio Defensive Rich', 'The active soothing cream, developed to strengthen the self-defence power of sentitive skin dried out by environmental aggressions.', 'https://www.bioderma.us/sites/us/files/styles/large/public/products/%7B40240%7D_%7B%7D_%7B28695W%7D.png.webp?itok=AJN60H3d', 1, 1, 1),
-  ('Atoderm Ultra Cream', 'Ultra-nourishing and moisturizing daily care. Strengthens the skin barrier.', 'https://www.bioderma.us/sites/us/files/styles/large/public/products/%7B63346%7D_%7BBIO_ATODERM_CREME%7D_%7B28065A%7D.png.webp?itok=HScAna60', 1, 2, 3),
-  ('Atoderm Hands & Nails', 'The nourishing cream for soft and supple hands. 8h hydration.', 'https://www.bioderma.us/sites/us/files/styles/large/public/products/%7B37150%7D_%7B%7D_%7B28070D%7D.png.webp?itok=eLB7GRS3', 1, 5, 3),
-  ('Hydrabio Toner', 'Hydrating, liquid toning lotion for dehydrated skin.', 'https://www.bioderma.us/sites/us/files/styles/large/public/products/%7B84601%7D_%7BBIO_HYDRABIO_TONIQUE%7D_%7B28379%7D.png.webp?itok=Y09x7Wf0', 1, 3, 2),
-  ('Atoderm Lip Balm', 'A daily moisturizing care that soothes, nourishes and repairs dry lips.', 'https://www.bioderma.us/sites/us/files/styles/large/public/products/%7B37252%7D_%7BBIO_ATODERM_BAUME_LEVRES%7D_%7B28095W%7D.png.webp?itok=RlHKpDnN', 1, 4, 3),
-  ('Sébium Mat Control', 'The care which mattifies and smoothes for a lastingly transformed skin.', 'https://www.bioderma.us/sites/us/files/styles/large/public/products/%7B38332%7D_%7BBIO_SEBIUM_MAT_CONTROL%7D_%7B28658B%7D.png.webp?itok=Iu7cKAx2', 1, 1, 4),
-  ('Sébium Exfoliating Gel', 'Purifying, exfoliating gel that provides the appearance of smoother skin.', 'https://www.bioderma.us/sites/us/files/styles/large/public/products/%7B38464%7D_%7BBIO_SEBIUM_GEL_GOMMANT%7D_%7B28625I%7D.png.webp?itok=mIRqGBuO', 1, 1, 4),
-  ('Cicabio Mains', 'The repairing balm with a second skin effect. Hands instant relief.', 'https://www.bioderma.us/sites/us/files/styles/large/public/2022-10/Cicabio-Mains-2022%20%282%29-min.png.webp?itok=X50R6UXp', 1, 5, 1),
-  ('Sensibio Micellar cleansing oil', 'Ecobiological cleansing oil with micellar technology to cleanse and take care of the skin.', 'https://www.bioderma.us/sites/us/files/styles/large/public/products/%7B107154%7D_%7BBIO_SENSIBIO_MICELLAR_CLEANSING_OIL%7D_%7B28701A%7D.png.webp?itok=IXte_WgP', 1, 3, 1),
-  ('Sébium Foaming Gel', 'The purifying gentle cleanser. Removes impurities, controls shine.', 'https://www.bioderma.us/sites/us/files/styles/large/public/products/%7B63370%7D_%7B%7D_%7B28664A%7D.png.webp?itok=6TVs_xXG', 1, 2, 4),
-  ('Atoderm Shower Oil', '24h hydration and immediate comfort right from the shower.', 'https://www.bioderma.us/sites/us/files/styles/large/public/products/Atoderm-Huile-de-douche-F1L-28138C-MAD-June24.png.webp?itok=EtHhzosw', 1, 2, 3);
+  ('Sensibio Defensive Rich', 'The active soothing cream, developed to strengthen the self-defence power of sentitive skin dried out by environmental aggressions.', 'https://res.cloudinary.com/dboemgriz/image/upload/v1739042344/sensibioRich_j16ny9.jpg', 1, 1, 1),
+  ('Atoderm Ultra Cream', 'Ultra-nourishing and moisturizing daily care. Strengthens the skin barrier.', 'https://res.cloudinary.com/dboemgriz/image/upload/v1739042357/atodermUltraCream_zqxj5l.jpg', 1, 2, 3),
+  ('Atoderm Hands & Nails', 'The nourishing cream for soft and supple hands. 8h hydration.', 'https://res.cloudinary.com/dboemgriz/image/upload/v1739014457/atodermCream_gpmwm0.jpg', 1, 5, 3),
+  ('Hydrabio Toner', 'Hydrating, liquid toning lotion for dehydrated skin.', 'https://res.cloudinary.com/dboemgriz/image/upload/v1739042360/hydrobioToner_fsiiyi.jpg', 1, 3, 2),
+  ('Atoderm Lip Balm', 'A daily moisturizing care that soothes, nourishes and repairs dry lips.', 'https://res.cloudinary.com/dboemgriz/image/upload/v1739042619/atodermLips_ol2flt.jpg', 1, 4, 3),
+  ('Sébium Mat Control', 'The care which mattifies and smoothes for a lastingly transformed skin.', 'https://res.cloudinary.com/dboemgriz/image/upload/v1739042622/sebiumMatControl_gb1leq.jpg', 1, 1, 4),
+  ('Sébium Exfoliating Gel', 'Purifying, exfoliating gel that provides the appearance of smoother skin.', 'https://res.cloudinary.com/dboemgriz/image/upload/v1739043149/sebiumExfoliant_fvuc2i.jpg', 1, 1, 4),
+  ('Cicabio Mains', 'The repairing balm with a second skin effect. Hands instant relief.', 'https://res.cloudinary.com/dboemgriz/image/upload/v1739043298/cicabioHandCream_rxzyd2.jpg', 1, 5, 1),
+  ('Sensibio Micellar cleansing oil', 'Ecobiological cleansing oil with micellar technology to cleanse and take care of the skin.', 'https://res.cloudinary.com/dboemgriz/image/upload/v1739043398/sensibioOil_oo2vcx.jpg', 1, 3, 1),
+  ('Sébium Foaming Gel', 'The purifying gentle cleanser. Removes impurities, controls shine.', 'https://res.cloudinary.com/dboemgriz/image/upload/v1739042953/sebiumGel_yze6ej.jpg', 1, 2, 4),
+  ('Atoderm Shower Oil', '24h hydration and immediate comfort right from the shower.', 'https://res.cloudinary.com/dboemgriz/image/upload/v1739014457/atodermOil_r4awg9.jpg', 1, 2, 3);
 `
 async function main() {
   console.log("seeding...");
